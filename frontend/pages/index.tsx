94852bbceba6f75be89bcda4fd8099f4ae990dda
Josh Tummala <josh.tummala@gmail.com>
2025-05-11 22:28:46 -0500

    Updates frontend to handle new logic

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ import { Survey } from '../components/Survey';
 const SurveyPage = () => {
   return (
     <div className="container mx-auto max-w-md h-screen">
-      <Survey />
+      <Survey surveyInstanceId="2b26de21-cd1f-4d4d-802c-3adebb31f8c4" />
     </div>
   );
 };
